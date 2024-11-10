2f401613f3018db421ab0a7df0fa6f05c1f6356c
Rushikesh Machhindra Khamkar <38386736+Rushikesh1234@users.noreply.github.com>
2024-11-09 21:31:37 -0500

    Add files via upload

--- utils/points_calculator.go
+++ utils/points_calculator.go
@@ -11,7 +11,6 @@ import (
 func CalculatePoints(receipt map[string]interface{}) int {
 	points := 0
 
-	// Retailer points: One point per alphanumeric character
 	retailer := receipt["retailer"].(string)
 	for _, ch := range retailer {
 		if unicode.IsLetter(ch) || unicode.IsDigit(ch) {
@@ -19,7 +18,6 @@ func CalculatePoints(receipt map[string]interface{}) int {
 		}
 	}
 
-	// Total points: Round dollar amount or multiple of 0.25
 	total, _ := strconv.ParseFloat(receipt["total"].(string), 64)
 	if total == math.Trunc(total) {
 		points += 50
@@ -28,11 +26,9 @@ func CalculatePoints(receipt map[string]interface{}) int {
 		points += 25
 	}
 
-	// Points for item pairs
 	items := receipt["items"].([]interface{})
 	points += (len(items) / 2) * 5
 
-	// Item description length multiple of 3
 	for _, item := range items {
 		itemMap := item.(map[string]interface{})
 		desc := strings.TrimSpace(itemMap["shortDescription"].(string))
@@ -42,13 +38,11 @@ func CalculatePoints(receipt map[string]interface{}) int {
 		}
 	}
 
-	// Purchase date: 6 points if the day is odd
 	date, _ := time.Parse("2006-01-02", receipt["purchaseDate"].(string))
 	if date.Day()%2 != 0 {
 		points += 6
 	}
 
-	// Purchase time: 10 points if between 2 PM and 4 PM
 	timeStr := receipt["purchaseTime"].(string)
 	purchaseTime, _ := time.Parse("15:04", timeStr)
 	if purchaseTime.Hour() >= 14 && purchaseTime.Hour() < 16 {
